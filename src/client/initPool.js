2a5b8043c2fd5c72eee7bbdd991567c07c6568b0
Paul Schaaf <paulsimonschaaf@gmail.com>
2020-11-08 23:29:20 +0100

    working initPool script

--- src/client/initPool.js
+++ src/client/initPool.js
@@ -4,7 +4,12 @@ import {
   PublicKey,
   LAMPORTS_PER_SOL,
   Transaction,
+  SystemProgram,
+  Account,
+  SYSVAR_RENT_PUBKEY,
 } from "@solana/web3.js";
+import { Token, TOKEN_PROGRAM_ID } from "@solana/spl-token";
+
 import { Store } from "./util/store";
 import { newAccountWithLamports } from "./util/new-account-with-lamports";
 import { sendAndConfirmTransaction } from "./util/send-and-confirm-transaction";
@@ -12,18 +17,6 @@ import { url } from "./url";
 
 // u64
 const POOL_REQUEST_TAG = [207, 196, 28, 205, 189, 108, 10, 34];
-
-const VAULT_SIGNER_NONCE = 0;
-const ASSETS_LENGTH = 2;
-const BORSH_ETF = [3, 0, 0, 0, 101, 116, 102];
-
-const INITIALIZE_POOL_REQUEST = [
-  VAULT_SIGNER_NONCE,
-  ASSETS_LENGTH,
-  ...BORSH_ETF,
-  ...[0, 0, 0, 0],
-];
-const REQUEST_INNER_INITIALIZE = [0, ...INITIALIZE_POOL_REQUEST];
 
 const initPool = async () => {
   const store = new Store();
@@ -47,17 +40,99 @@ const initPool = async () => {
     LAMPORTS_PER_SOL * 10
   );
 
+  console.log("Creating token A mint account...");
+  const tokenAMintAccount = await Token.createMint(
+    connection,
+    masterAccount,
+    masterAccount.publicKey,
+    masterAccount.publicKey,
+    0,
+    TOKEN_PROGRAM_ID
+  );
+
+  console.log("Creating token B mint account...");
+  const tokenBMintAccount = await Token.createMint(
+    connection,
+    masterAccount,
+    masterAccount.publicKey,
+    masterAccount.publicKey,
+    0,
+    TOKEN_PROGRAM_ID
+  );
+
+  const poolAccount = new Account();
+  const createPoolAccountInstruction = SystemProgram.createAccount({
+    fromPubkey: masterAccount.publicKey,
+    newAccountPubkey: poolAccount.publicKey,
+    lamports: 4 * LAMPORTS_PER_SOL,
+    space: 300,
+    programId,
+  });
+
+  const vaultAuthority = await PublicKey.findProgramAddress(
+    [poolAccount.publicKey.toBuffer()],
+    programId
+  );
+
+  const tokenATokenAccount = await tokenAMintAccount.createAccount(
+    vaultAuthority[0]
+  );
+
+  const tokenBTokenAccount = await tokenBMintAccount.createAccount(
+    vaultAuthority[0]
+  );
+
+  console.log("Creating pool token mint account...");
+  const poolTokenMintAccount = await Token.createMint(
+    connection,
+    masterAccount,
+    vaultAuthority[0],
+    vaultAuthority[0],
+    0,
+    TOKEN_PROGRAM_ID
+  );
+
+  const VAULT_SIGNER_NONCE = vaultAuthority[1];
+  const ASSETS_LENGTH = 2;
+  const BORSH_ETF = [3, 0, 0, 0, 101, 116, 102];
+
+  const INITIALIZE_POOL_REQUEST = [
+    VAULT_SIGNER_NONCE,
+    ASSETS_LENGTH,
+    ...BORSH_ETF,
+    ...[0, 0, 0, 0],
+  ];
+  const REQUEST_INNER_INITIALIZE = [0, ...INITIALIZE_POOL_REQUEST];
+
   const initPoolInstruction = new TransactionInstruction({
-    keys: [],
+    keys: [
+      { pubkey: poolAccount.publicKey, isSigner: false, isWritable: true },
+      {
+        pubkey: poolTokenMintAccount.publicKey,
+        isSigner: false,
+        isWritable: true,
+      },
+      { pubkey: tokenATokenAccount, isSigner: false, isWritable: true },
+      { pubkey: tokenBTokenAccount, isSigner: false, isWritable: true },
+      { pubkey: vaultAuthority[0], isSigner: false, isWritable: false },
+      { pubkey: SYSVAR_RENT_PUBKEY, isSigner: false, isWritable: false },
+    ],
     programId,
     data: [...POOL_REQUEST_TAG, ...REQUEST_INNER_INITIALIZE],
   });
 
+  console.log("Initializing pool...");
   await sendAndConfirmTransaction(
     connection,
-    new Transaction().add(initPoolInstruction),
-    masterAccount
+    new Transaction().add(createPoolAccountInstruction, initPoolInstruction),
+    masterAccount,
+    poolAccount
   );
+
+  console.log("\x1b[32m%s\x1b[0m", "Pool initialized!");
+  await store.save("pool.json", {
+    poolKey: poolAccount.publicKey.toBase58(),
+  });
 };
 
 initPool()
